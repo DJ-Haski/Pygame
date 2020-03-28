f9550f8135e98815f90816de78706f6b921109ca
Roman Biryukov <49877012+DJ-Haski@users.noreply.github.com>
2020-03-28 21:33:19 +0600

    v3
    error again (

--- gamev2.py
+++ gamev2.py
@@ -1,4 +1,4 @@
-import pygame#, time
+import pygame
 pygame.init()
 swidth=600
 sheight=600
@@ -11,12 +11,15 @@ width = 40
 height = 60
 speed = 5
 
+isJump=False
+jumpCount=10
+
 run = True
-while run:
+while run is True:
     pygame.time.delay(50)
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT():
+        if event.type == pygame.QUIT(): # тут ошибка, которую я не могу пофиксить :(
             run = False
 
     keys=pygame.key.get_pressed()
@@ -24,16 +27,27 @@ while run:
         x -= speed
     if keys[pygame.K_RIGHT] and x<swidth-width-speed:
         x += speed
-    if keys[pygame.K_UP] and y>speed:
-        y -= speed
-    if keys[pygame.K_DOWN] and y<sheight-height-speed:
-        y += speed
+    if not(isJump):
+        if keys[pygame.K_UP] and y>speed:
+            y -= speed
+        if keys[pygame.K_DOWN] and y<sheight-height-speed:
+            y += speed
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            y -= (jumpCount**2)/2
+            jumpCount -= 1
+        else:
+            isJump=False
+            jumpCount=10
+
 
     window.fill((0,0,0))
     pygame.draw.rect(window,(255,0,0),(x,y,width,height))
     pygame.display.update()
 
-pygame.quit()
+#pygame.quit()
 
 
 
