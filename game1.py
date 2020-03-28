7bd2979633ac1a0490ca4243aa755cbc880a43ab
Roman Biryukov <49877012+DJ-Haski@users.noreply.github.com>
2020-03-28 20:25:19 +0600

    v2

--- game1.py
+++ game1.py
@@ -3,7 +3,12 @@ import pygame, time
 pygame.init()
 window=pygame.display.set_mode((600,300))
 myfont= pygame.font.SysFont("Roboto", 60)
-label = myfont.render("hello, pygame!",1,(255,255,0))
+label = myfont.render("Hello, pygame!",1,(255,255,0))
+background = pygame.Surface(window.get_size())
+background.fill((55,60,255))
+background = background.convert()
+window.blit(background, (0, 0))
+
 window.blit(label,(100,100))
 pygame.display.update()
 time.sleep(5)
