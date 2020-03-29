f30e4c7f56db28dc22c49968788a6b25d6b21e16
Roman Biryukov <49877012+DJ-Haski@users.noreply.github.com>
2020-03-29 19:15:14 +0600

    V3

--- gamev2.py
+++ gamev2.py
@@ -1,7 +1,7 @@
 import pygame
 pygame.init()
-swidth=600
-sheight=600
+swidth=901
+sheight=901
 window=pygame.display.set_mode((swidth,sheight))
 pygame.display.set_caption("First Game in Python")
 myfont= pygame.font.SysFont("Roboto", 40)
@@ -15,6 +15,9 @@ y = 280
 width = 40
 height = 40
 speed = 5
+R=0
+G=0
+B=0
 
 isJump=False
 jumpCount=10
@@ -24,7 +27,7 @@ while run is True:
     pygame.time.delay(50)
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: # тут ошибка, которую я не могу пофиксить :(
+        if event.type == pygame.QUIT: # тут ошибка, которую я не могу пофиксить :( // уже пофиксил
             run = False
 
     keys=pygame.key.get_pressed()
@@ -52,9 +55,17 @@ while run is True:
             jumpCount=10
 
 
-    window.fill((0,0,0))
-    pygame.draw.circle(window,(238,130,238),(300,300),40)
-    #pygame.draw.polygon(window, (255, 255, 255), ((250, 0),(125,125),(0,250)))
+    window.fill((R,G,B))
+
+    for point in range(0, 900, 64):  # range(start, stop, step)
+        pygame.draw.line(window, (255, 0, 255), (0, 0), (450, point), 2)
+        pygame.draw.line(window, (0, 255, 255), (900, 0), (450, point), 2)
+    for point in range(0, 900, 64):  # range(start, stop, step)
+        pygame.draw.line(window, (255, 0, 0), (0, 900), (450, point), 2)
+        pygame.draw.line(window, (0, 255, 0), (900, 900), (450, point), 2)
+    #pygame.draw.circle(window, (238, 130, 238), (300, 300), 40)
+    #pygame.draw.arc(window, (0, 255, 0), (0, -900, 1800, 1800), 3.14, (0.75 * 6.28))
+    #pygame.draw.polygon(window, (255, 0, 0), ((500, 175), (600, 500), (350, 300), (650, 300), (400, 500)), 10)
     pygame.draw.rect(window, (255, 255, 255), (x, y, width, height))
     pygame.display.update()
 pygame.time.delay(1000)
